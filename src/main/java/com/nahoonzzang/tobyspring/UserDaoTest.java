062ec29b4720d04cd8123c7b5990b2cac5370823
nahoonzzang <0828namjin@gmail.com>
2018-06-10 12:44:48 +0900

    1.5 Spring-IoC

--- src/main/java/com/nahoonzzang/tobyspring/UserDaoTest.java
+++ src/main/java/com/nahoonzzang/tobyspring/UserDaoTest.java
@@ -1,13 +1,21 @@
 package com.nahoonzzang.tobyspring;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class UserDaoTest {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
 
-        UserDao userDao = new DaoFactory().userDao();
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(DaoFactory.class);
+
+        // getBean() 메소드는 ApplicationContext가 관리하는 오브젝트를 요청하는 메소드
+        // "userDao"는 ApplicationContext에 등록된 빈의 이름(빈의 이름은 팩토리의 @Bean어노테이선이 붙은 메소드의 이름)
+        // 즉, DaoFactory의 userDao() 메소드를 호출해서 그 결과를 가져온다고 생각하면 된다.
+        UserDao userDao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
         user.setId("0828namjin");
